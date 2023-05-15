07c9043102f7162db326188b2c8817551ed6392b
Abdelhady Mohamed <engabdelhadymohamed@gmail.com>
2023-05-15 16:50:32 +0300

    Create : Migration to edit facility column to use Facility enum

---
+++ GetSit/Migrations/20230515134836_HallFacilityEnum.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GetSit.Migrations
+{
+    /// <inheritdoc />
+    public partial class HallFacilityEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "HallFacility");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Facility",
+                table: "HallFacility",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Facility",
+                table: "HallFacility");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "HallFacility",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
