162c358272c18d725c9d035a9cd379e8cd041f16
Abdelhady Mohamed <engabdelhadymohamed@gmail.com>
2023-05-15 16:59:35 +0300

    Create : Migration fix Space has Halls relation

---
+++ GetSit/Migrations/20230515135805_addSpaceIdToHall.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GetSit.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSpaceIdToHall : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SpaceHall_Space_SpaceId",
+                table: "SpaceHall");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SpaceId",
+                table: "SpaceHall",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SpaceHall_Space_SpaceId",
+                table: "SpaceHall",
+                column: "SpaceId",
+                principalTable: "Space",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SpaceHall_Space_SpaceId",
+                table: "SpaceHall");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SpaceId",
+                table: "SpaceHall",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SpaceHall_Space_SpaceId",
+                table: "SpaceHall",
+                column: "SpaceId",
+                principalTable: "Space",
+                principalColumn: "Id");
+        }
+    }
+}
