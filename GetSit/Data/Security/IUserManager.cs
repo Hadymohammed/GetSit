9696ca676fbbd405bb87ced471a8a6d920e2f3d4
Abdelhady Mohamed <engabdelhadymohamed@gmail.com>
2023-05-21 04:24:36 +0300

    Add : UserManager for Auth services

---
+++ GetSit/Data/Security/IUserManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+namespace GetSit.Data.Security
+{
+    public interface IUserManager
+    {
+        Task SignIn<T>(HttpContext httpContext, T user, bool isPersistent = false) where T : IAbstractUser;
+        Task SignOut(HttpContext httpContext);
+        int GetCurrentUserId(HttpContext httpContext);
+        IAbstractUser GetCurrentUser(HttpContext httpContext);
+        IEnumerable<Claim> GetUserClaims<T>(T user) where T : IAbstractUser;
+        public string GetUserRole(HttpContext httpContext);
+    }
+}
