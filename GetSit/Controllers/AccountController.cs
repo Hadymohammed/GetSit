a18266ee064237fe3f74c92e109034c1c320208e
Abdelhady Mohamed <engabdelhadymohamed@gmail.com>
2023-05-21 04:25:28 +0300

    Create : Auth actions all works

--- GetSit/Controllers/AccountController.cs
+++ GetSit/Controllers/AccountController.cs
@@ -5,15 +5,19 @@ using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel;
 using GetSit.Data;
 using Microsoft.Win32;
+using GetSit.Data.Security;
+using Microsoft.AspNetCore.Authorization;
 
 namespace GetSit.Controllers
 {
     public class AccountController : Controller
     {
         AppDBcontext _context;
-        public AccountController(AppDBcontext context)
+        private IUserManager _userManager;
+        public AccountController(AppDBcontext context, IUserManager userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
@@ -53,13 +57,11 @@ namespace GetSit.Controllers
                     /*context sign in */
                     break;
                 case UserRole.Customer:
-                    var customer = new Customer()
-                    {
-                        Email = login.Email,
-                        Password = login.Password,/*Here password should be decrypted*/
-                    };
-                    /*Varify user : macthed email and password*/
+        
+                    var user = _context.Customer.Where(c=>c.Email==login.Email&&c.Password==login.Password).FirstOrDefault();/*Varify user : macthed email and password*/
                     /*Get DB user*/
+                    _userManager.SignIn(HttpContext, user);
+                    return RedirectToAction("CustomerProfile", "Account");
                     /*context sign in */
                     break;
                 default:
@@ -67,12 +69,18 @@ namespace GetSit.Controllers
             }
             return View();
         }
+        [HttpPost]
+        public IActionResult logout()
+        {
+            _userManager.SignOut(HttpContext);
+            return RedirectToAction("Login");
+        }
         public IActionResult Register()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Register(RegisterVM register)
+        public async Task<IActionResult> Register(RegisterVM register)
         {
             if (!ModelState.IsValid)
             {
@@ -88,10 +96,20 @@ namespace GetSit.Controllers
                         LastName = register.LastName,
                         Email = register.Email,
                         PhoneNumber = register.PhoneNumber,
+                        Birthdate = register.Birthdate,
                         Password = register.Password,/*Here password should be hashed*/
                     };
-                    _context.SystemAdmin.Add(admin);
-                    /*context sign in*/
+                    try
+                    {
+                    await _context.SystemAdmin.AddAsync(admin);
+                    _context.SaveChanges();
+                    await _userManager.SignIn(HttpContext, admin);
+                    return RedirectToAction("AdminProfile", "Account");
+                    }
+                    catch (Exception error)
+                    {
+                        return View(register);
+                    }
                     break;
                 case UserRole.Provider:
                     var provider = new SpaceEmployee()
@@ -100,10 +118,20 @@ namespace GetSit.Controllers
                         LastName = register.LastName,
                         Email = register.Email,
                         PhoneNumber = register.PhoneNumber,
+                        Birthdate = register.Birthdate,
                         Password = register.Password,/*Here password should be hashed*/
                     };
-                    _context.SpaceEmployee.Add(provider);
-                    /*context sign in*/
+                    try
+                    {
+                    await _context.SpaceEmployee.AddAsync(provider);
+                    _context.SaveChanges();
+                    await _userManager.SignIn(HttpContext, provider);
+                    return RedirectToAction("ProviderProfile", "Account");
+                    }
+                    catch (Exception error)
+                    {
+                        return View(register);
+                    }
                     break;
                 case UserRole.Customer:
                     var customer = new Customer()
@@ -112,15 +140,51 @@ namespace GetSit.Controllers
                         LastName = register.LastName,
                         Email = register.Email,
                         PhoneNumber = register.PhoneNumber,
+                        CustomerType=CustomerType.Registered,
+                        Birthdate=register.Birthdate,
                         Password = register.Password,/*Here password should be hashed*/
                     };
-                    _context.Customer.Add(customer);
-                    /*context sign in*/
+                    try
+                    {
+                       await _context.Customer.AddAsync(customer);
+                        _context.SaveChanges();
+                       await _userManager.SignIn(HttpContext, customer);
+                       return RedirectToAction("CustomerProfile");
+                    }
+                    catch (Exception error)
+                    {
+                        return View(register);
+                    }
                     break;
                 default:
+                    return View(register);
                     break;
             }
+            return View(register);
+        }
+        [Authorize(Roles = "Admin")]//error enum must be used
+        public IActionResult AdminProfile()
+        {
+            var user = _userManager.GetCurrentUser(HttpContext);
+            return View(user);
+        }
+        [Authorize(Roles = "Customer")]//error enum must be used
+        public IActionResult CustomerProfile()
+        {
+            var user = _userManager.GetCurrentUser(HttpContext);
+            return View(user);
+        }
+        [Authorize(Roles = "Provider")]//error enum must be used
+        public IActionResult ProviderProfile()
+        {
+            var user = _userManager.GetCurrentUser(HttpContext);
+            return View(user);
+        }
+        public IActionResult AccessDenied()
+        {
             return View();
         }
+
+
     }
 }
