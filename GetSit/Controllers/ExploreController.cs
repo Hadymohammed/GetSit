8619372c5f5c00acda92c075351af7f21a99f3bd
wannan24 <marwanwannan24@gmail.com>
2023-05-23 15:55:10 +0300

    SearchController

---
+++ GetSit/Controllers/ExploreController.cs
@@ -0,0 +1,39 @@
+﻿using GetSit.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GetSit.Controllers
+{
+    public class ExploreController : Controller
+    {
+        public raedonly ExploreController()
+        {
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Search(string query)
+        {
+           
+            return View(results);
+        }
+        public ActionResult Searchh(string query)
+        {
+            var searchResults = GetSearchResults(query);
+
+            return View(searchResults);
+        }
+        private List<Space> GetSearchResults(string query)
+        {
+            
+            var Spaces = AppDBContext.Spaces.Where(p => p.Name.Contains(query)).ToList();
+
+            return Spaces;
+        }
+
+    }
+
+}
