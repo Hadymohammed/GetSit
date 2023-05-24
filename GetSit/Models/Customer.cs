ea9c8b82d07282df1021df9f5a1c50d2aaff1ac7
Abdelhady Mohamed <engabdelhadymohamed@gmail.com>
2023-05-24 17:58:15 +0300

    Update format Customer.cs

--- GetSit/Models/Customer.cs
+++ GetSit/Models/Customer.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 using System.Security.Principal;
+
 namespace GetSit.Models
 {
     public class Customer:IAbstractUser
