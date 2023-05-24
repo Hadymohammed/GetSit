c9e3ef12f7b107e0b4887c37e277d2b41785880c
wannan24 <marwanwannan24@gmail.com>
2023-05-24 17:11:53 +0300

    BookingController

--- GetSit/Controllers/BookingController.cs
+++ GetSit/Controllers/BookingController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GetSit.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GetSit.Controllers
 {
     public class BookingController : Controller
     {
+        AppDBcontext _context;
+        public BookingController( AppDBcontext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public IActionResult HallBookingDetails(int HallId)
+        {
+            var Hall = _context.SpaceHall.Where(h => h.Id == HallId).FirstOrDefault();
+            return View(HallId);
         }
     }
 }
