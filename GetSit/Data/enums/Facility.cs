ae52fa2114bcd50ae5cc94c08346a45bac1de6b5
Abdelhady Mohamed <engabdelhadymohamed@gmail.com>
2023-05-24 23:20:20 +0300

    Add :Facility & WeekDay enums

---
+++ GetSit/Data/enums/Facility.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GetSit.Data.enums
+{
+    public enum Facility
+    {
+        [Display(Name = "Free Wifi")]
+        FreeWifi = 1,
+        [Display(Name = "Unlimited Coffee")]
+        UnlimitedCoffee,
+        [Display(Name = "OpenAir View")]
+        OpenAirView,
+        [Display(Name = "Unlimited Internet")]
+        UnlimitedInternet,
+    }
+}
