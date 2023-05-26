5ad88f01180bec6c248005c2027f4231eca8a403
Abdelhady Mohamed <engabdelhadymohamed@gmail.com>
2023-05-26 05:00:18 +0300

    Add : SpaceManagement profile&sections nav& halls & services

---
+++ GetSit/Controllers/SpaceManagementController.cs
@@ -0,0 +1,49 @@
+﻿using GetSit.Data;
+using GetSit.Data.enums;
+using GetSit.Data.Security;
+using GetSit.Data.ViewModels;
+
+using GetSit.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GetSit.Controllers
+{
+    [Authorize(Roles = "Provider")]//Convert UserRole to class
+    public class SpaceManagementController : Controller
+    {
+        readonly IUserManager _userManager;
+        readonly AppDBcontext _context;
+        public SpaceManagementController(IUserManager userManager,AppDBcontext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var SpaceId="";
+            if(HttpContext.Request.Cookies.Where(c=>c.Key=="SpaceId").FirstOrDefault().Value is null)
+            {
+                var providerId = _userManager.GetCurrentUserId(HttpContext);
+                var provider = _context.SpaceEmployee.Where(e => e.Id == providerId).FirstOrDefault();
+                SpaceId = provider.SpaceId.ToString();
+                if(SpaceId != String.Empty)
+                    HttpContext.Response.Cookies.Append("SpaceId", SpaceId);
+            }
+            else
+            {
+                SpaceId = HttpContext.Request.Cookies.Where(c => c.Key == "SpaceId").FirstOrDefault().Value;
+            }
+            Space space = _context.Space.Include(s=>s.Photos).Where(s => s.Id.ToString() == SpaceId).FirstOrDefault();
+            SpaceManagementVM viewModel = new ()
+            {
+                Space=space,
+                Halls = _context.SpaceHall.Include(h=>h.HallPhotos).Where(h=>h.SpaceId.ToString()==SpaceId).ToList(),
+                Services= _context.SpaceService.Include(h => h.ServicePhotos).Where(h => h.SpaceId.ToString() == SpaceId).ToList(),
+                Employees= _context.SpaceEmployee.Where(h => h.SpaceId.ToString() == SpaceId).ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ GetSit/Data/ViewModels/SpaceManagementVM.cs
@@ -0,0 +1,12 @@
+﻿using GetSit.Models;
+
+namespace GetSit.Data.ViewModels
+{
+    public class SpaceManagementVM
+    {
+        public Space Space { get; set; }
+        public List<SpaceHall> Halls { get; set; }
+        public List<SpaceService> Services { get; set; }
+        public List<SpaceEmployee> Employees { get; set; }
+    }
+}
