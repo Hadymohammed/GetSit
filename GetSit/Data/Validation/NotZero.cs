4c31011244e27b31ce303a1ce24b10c3d4e9637b
Abdelhady Mohamed <engabdelhadymohamed@gmail.com>
2023-06-01 23:45:02 +0300

    Add : NotZero validation

---
+++ GetSit/Data/Validation/NotZero.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GetSit.Data.Validation
+{
+    public class NotZero: ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            if (value?.ToString() == "0")
+                return false;
+            return true;
+        }
+    }
+}
