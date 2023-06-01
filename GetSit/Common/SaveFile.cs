313dfffe62725b23e136b1ede65f324e3391bc39
Abdelhady Mohamed <engabdelhadymohamed@gmail.com>
2023-06-01 23:44:45 +0300

    Add : savefile services

---
+++ GetSit/Common/SaveFile.cs
@@ -0,0 +1,32 @@
+﻿using GetSit.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace GetSit.Common
+{
+    public class SaveFile
+    {
+        public static async Task<string> HallPhoto(IFormFile file,string SpaceName,int hallId,int PhotoNumber)
+        {
+            try
+            {
+                if (file != null && file.Length > 0)
+                {
+                    string ext = Path.GetExtension(file.FileName);
+                    var fileName =SpaceName+"_"+hallId.ToString()+"_"+PhotoNumber.ToString()+ext;
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\resources\\HallPhotos", fileName);//NeedCompletePath
+                    using (var fileSrteam = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileSrteam);
+                    }
+                    return ".\\resources\\HallPhotos\\"+fileName;
+                }
+                return null;
+            }catch(Exception e)
+            {
+                return null;
+            }
+            return null;
+        }
+    }
+}
