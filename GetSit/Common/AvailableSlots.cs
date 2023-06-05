242722c59171b81ef0084383fe724accf1bacb08
Abdelhady Mohamed <engabdelhadymohamed@gmail.com>
2023-06-06 00:18:06 +0300

    Fix : booking.DesiredDate

--- GetSit/Common/AvailableSlots.cs
+++ GetSit/Common/AvailableSlots.cs
@@ -17,16 +17,15 @@ namespace GetSit.Common
         }
          public List<Tuple<TimeSpan,bool>> GetAvailableSlotsForDay(int hallId, DateTime date)
         {
-            var bookings = /*_context.Booking.Include(m=>m.BookingHalls && BookingDate == date).ToList();*/
-            _context.Booking
-             .Where(b => b.BookingHalls.Any(h => h.Id == hallId && b.BookingDate == date))
-             .ToList();
+            var bookings = _context.Booking
+                           .Where(b => b.BookingHalls.Any(h => h.HallId == hallId && b.DesiredDate == date))
+                           .ToList();
 
             var space = _context.Space.Where(s => s.Halls.Any(h => h.Id == hallId)).FirstOrDefault();
 
             // check if the day is out of space working days
 
-            WeekDay currentDay = (WeekDay)date.DayOfWeek;
+            var currentDay = date.DayOfWeek;
 
             SpaceWorkingDay? obj = _context.SpaceWorkingDay.Where(h => h.SpaceId == space.Id && h.Day == currentDay).FirstOrDefault();
             //Form the default 96 day time slots
@@ -42,33 +41,38 @@ namespace GetSit.Common
                 // all slots are = false
                return timeSlots;
             }
-
-            TimeSpan startTime = TimeSpan.FromHours(obj.OpeningTime.TotalHours); // Start time for the day
-            TimeSpan endTime = TimeSpan.FromHours(obj.ClosingTime.TotalHours); // End time for the day
-            TimeSpan timeSlot = TimeSpan.FromMinutes(15); // Length of each time slot
-            var slotIdx = (int)(startTime.TotalMinutes/15);
-
-            for(;slotIdx<(int)(endTime.TotalMinutes/15);slotIdx++)
+            try
             {
-                var endTimeSlot = timeSlots[slotIdx].Item1.Add(timeSlot); // End time for the time slot
-                                                          // Check if the time slot is available
-                var isAvailable = true;
-                foreach (var booking in bookings)
+                TimeSpan startTime = TimeSpan.FromHours(obj.OpeningTime.TotalHours); // Start time for the day
+                TimeSpan endTime = TimeSpan.FromHours(obj.ClosingTime.TotalHours); // End time for the day
+                TimeSpan timeSlot = TimeSpan.FromMinutes(15); // Length of each time slot
+                var slotIdx = (int)(startTime.TotalMinutes / 15);
+
+                for (; slotIdx < (int)(endTime.TotalMinutes / 15); slotIdx++)
                 {
-                    var EndTime = booking.StartTime.Add(TimeSpan.FromHours(booking.NumberOfHours));
-                    if (booking.StartTime < endTimeSlot && EndTime > timeSlots[slotIdx].Item1)
+                    var endTimeSlot = timeSlots[slotIdx].Item1.Add(timeSlot); // End time for the time slot
+                                                                              // Check if the time slot is available
+                    var isAvailable = true;
+                    foreach (var booking in bookings)
                     {
-                        isAvailable = false;
-                        break;
+                        var EndTime = booking.StartTime.Add(TimeSpan.FromHours(booking.NumberOfHours));
+                        if (booking.StartTime <= endTimeSlot && EndTime > timeSlots[slotIdx].Item1)
+                        {
+                            isAvailable = false;
+                            break;
+                        }
                     }
-                }
 
-                // If the time slot is available, add it to the list of available time slots
-                if (isAvailable)
-                {
-                    timeSlots[slotIdx] = new Tuple<TimeSpan, bool>(TimeSpan.FromMinutes(slotIdx*15), true);
-                }
+                    // If the time slot is available, add it to the list of available time slots
+                    if (isAvailable)
+                    {
+                        timeSlots[slotIdx] = new Tuple<TimeSpan, bool>(TimeSpan.FromMinutes(slotIdx * 15), true);
+                    }
 
+                }
+            }catch(Exception e)
+            {
+                return timeSlots;
             }
             return timeSlots;
         }
