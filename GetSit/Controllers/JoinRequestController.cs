d7f208730354012bd655b5da41ecb7ff10311875
Abdelhady Mohamed <engabdelhadymohamed@gmail.com>
2023-06-13 03:57:15 +0300

    Update : Actions to match registration process

--- GetSit/Controllers/JoinRequestController.cs
+++ GetSit/Controllers/JoinRequestController.cs
@@ -8,34 +8,48 @@ using Microsoft.AspNetCore.Authorization;
 using System.Net.Mail;
 using System.Net;
 using Microsoft.Win32;
+using GetSit.Data.Services;
+using Newtonsoft.Json;
 
 namespace GetSit.Controllers
 {
     public class JoinRequestController : Controller
     {
+        #region Dependencies
         private readonly AppDBcontext _context;
         private readonly IUserManager _userManager;
-        public JoinRequestController(AppDBcontext context, IUserManager userManager)
+        private readonly ISpaceEmployeeService _providerService;
+        private readonly ISpaceService _spaceService;
+        public JoinRequestController(AppDBcontext context,
+            IUserManager userManager,
+            ISpaceEmployeeService spaceEmployeeService,
+            ISpaceService spaceService)
         {
             _context = context;
             _userManager = userManager;
+            _providerService = spaceEmployeeService;
+            _spaceService = spaceService;
         }
-
+        #endregion
         [HttpGet]
         public async Task<IActionResult> Index()
         {
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> RequestToJoin(JoinRequestVM viewModel)
+        public async Task<IActionResult> Index(ProviderRegisterVM viewModel)
         {
             if (!ModelState.IsValid) 
             {
                 return View(viewModel);
             }
-            RandomPassword rand = new RandomPassword(_context);
-            string password = rand.GenerateRandomPassword(8);
+            if(_providerService.GetByEmail(viewModel.Email) != null)
+            {
+                ModelState.AddModelError("Email", "This email already has an account.");
+                return View(viewModel);
+            }
 
+            string password= RandomPassword.GenerateRandomPassword(8); 
             var Manager = new SpaceEmployee
             {
                 FirstName = viewModel.FirstName,
@@ -46,31 +60,16 @@ namespace GetSit.Controllers
                 Birthdate = viewModel.Birthdate,
                 IsApproved = false,
             };
-            var Space = new Space
+            HttpContext.Session.SetString("RegisterModel", JsonConvert.SerializeObject(Manager));
+
+            var otpVm = new OTPVM()
             {
-                Name = viewModel.SpaceName,
-                Bio = viewModel.SpaceBio,
-                Country = viewModel.SpaceCountry,
-                City = viewModel.SpaceCity,
-                Street = viewModel.SpaceStreet,
-                GPSLocation = viewModel.SpaceGPSLocation,
-                IsFast = viewModel.IsFast,
-                IsApproved = false,
-
+                Email=viewModel.Email,
+                Phone=viewModel.PhoneNumber
             };
-            Manager.Space = Space;
-            Manager.SpaceId = Space.Id;
-            await _context.SpaceEmployee.AddAsync(Manager);
-            await _context.Space.AddAsync(Space);
-            _context.SaveChanges();
-
-            var otpVm = new OTPVM();
-            otpVm.Email = viewModel.Email;
-            otpVm.Phone = viewModel.PhoneNumber;
-
             return RedirectToAction("PhoneOTP", otpVm);
         }
-
+        #region OTP verfication
         [HttpGet]
         public IActionResult PhoneOTP(OTPVM? otpVm)
         {
@@ -94,8 +93,6 @@ namespace GetSit.Controllers
 
             return RedirectToAction("EmailOTP", otp);
         }
-
-
         [HttpGet]
         public IActionResult EmailOTP(OTPVM? otpVm)
         {
@@ -104,7 +101,6 @@ namespace GetSit.Controllers
             OTPServices.SendEmailOTP(HttpContext, otpVm.Email);
             return View(otpVm);
         }
-
         [HttpPost]
         public async Task<IActionResult> EmailOTPAsync(OTPVM otp)
         {
@@ -117,9 +113,70 @@ namespace GetSit.Controllers
                 ModelState.AddModelError("OTP", "InValid Code");
                 return View(otp);
             }
-            return RedirectToAction("RequestToJoin");
+            return RedirectToAction("SpaceDetails");
         }
+        #endregion
+        
+        #region Space info
+        [HttpGet]
+        public IActionResult SpaceDetails()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> SpaceDetails(AddSpaceVM spaceVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(spaceVM);
+            }
+            var stringUser = HttpContext.Session.GetString("RegisterModel");
+            if (stringUser == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var manager = JsonConvert.DeserializeObject<SpaceEmployee>(stringUser) as SpaceEmployee;
 
+            var space = new Space
+            {
+                Name = spaceVM.SpaceName,
+                Bio = spaceVM.SpaceBio,
+                Country = spaceVM.SpaceCountry,
+                City = spaceVM.SpaceCity,
+                Street = spaceVM.SpaceStreet,
+                GPSLocation = spaceVM.SpaceGPSLocation,
+                IsFast = true,
+                IsApproved = false,
+                BankAccount="None"
+            };
+            await _spaceService.AddAsync(space);
+            manager.SpaceId = space.Id;
+            await _providerService.AddAsync(manager);
+
+            return RedirectToAction("Success",new JoinRequestVM()
+            {
+                FirstName=manager.FirstName,
+                LastName=manager.LastName,
+                Email=manager.Email,
+                PhoneNumber=manager.PhoneNumber,
+                Birthdate=manager.Birthdate,
+                SpaceName=space.Name,
+                SpaceBio=space.Bio,
+                SpaceCountry=space.Country,
+                SpaceCity=space.City,
+                SpaceStreet=space.Street,
+                SpaceGPSLocation=space.GPSLocation
+            });
+        }
+        #endregion Space info
+        [HttpGet]
+        public IActionResult Success(JoinRequestVM vm)
+        {
+            if (vm == null)
+                return NotFound();
+            return View(vm);
+        }
         [Authorize(Roles = "Admin")]
         public async Task<IActionResult> ApproveRequest(int SpaceID, int ManagerID)
         {
