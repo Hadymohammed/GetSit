651d919efc1ed602c3a0b320309482d47167084a
unknown <manarkhaled2510@gmail.com>
2023-06-16 18:52:38 +0300

    fix merge error

---
+++ GetSit/Data/Services/HallRequestService.cs
@@ -0,0 +1,44 @@
+﻿using GetSit.Data.enums;
+using GetSit.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace GetSit.Data.Services
+{
+    public class HallRequestService : IHallRequestService
+    {
+        readonly AppDBcontext _context;
+        public HallRequestService(AppDBcontext context) 
+        {
+            _context = context;
+        }
+        public void AddRequest(HallRequest request)
+        {
+            _context.Add(request);
+            _context.SaveChanges(); 
+        }
+        public void DeleteRequest(HallRequest request)
+        {
+            _context.Remove(request);
+            _context.SaveChanges();
+        }
+        public void UpdateRequest(HallRequest request)
+        {
+            _context.Update(request);
+            _context.SaveChanges();
+        }
+
+        public List<HallRequest> GetPendingBySpaceId(int spaceId, params Expression<Func<HallRequest, object>>[] includeProperties)
+        {
+            IQueryable<HallRequest> query = _context.Set<HallRequest>();
+            query = includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+            return query.Where(n => n.Hall.SpaceId == spaceId && n.Status != ReqestStatus.Accepted).ToList();
+        }
+        public HallRequest GetById(int RequestId)
+        {
+            var ret=_context.HallRequest.Where(h => h.Id == RequestId).Single();
+            return ret;
+        }
+
+    }
+}
--- GetSit/Data/Services/ISpaceHallService.cs
+++ GetSit/Data/Services/ISpaceHallService.cs
@@ -8,6 +8,8 @@ namespace GetSit.Data.Services
     {
         public Task<IEnumerable<SpaceHall>> GetBySpaceName(String Key);
         public List<SpaceHall> GetBySpaceId(int spaceId, params Expression<Func<SpaceHall, object>>[] includeProperties);
+        public void UpdateHall(SpaceHall Hall);
 
+        public List<SpaceHall> GetAcceptedBySpaceId(int spaceId, params Expression<Func<SpaceHall, object>>[] includeProperties);
     }
 }
