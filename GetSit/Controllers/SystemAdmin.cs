a1f3147516a433a7a253582a5d7255b7670ed56a
unknown <manarkhaled2510@gmail.com>
2023-06-16 17:03:38 +0300

    System admin controller

---
+++ GetSit/Controllers/SystemAdmin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GetSit.Controllers
+{
+    public class SystemAdmin : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
