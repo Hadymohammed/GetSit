5aa030c12defb1e848e310b9401f8b5aa1d09247
Abdelhady Mohamed <engabdelhadymohamed@gmail.com>
2023-06-20 03:51:30 +0300

    Add : Consts class

---
+++ GetSit/Common/Consts.cs
@@ -0,0 +1,10 @@
+﻿namespace GetSit.Common
+{
+    public class Consts
+    {
+        //Photos
+        public const string userProfilePhotoHolder = "./resources/site/user1.jpg";
+        public const string SpaceLogoHolder = "./resources/site/logo-social.png";
+        public const string SpaceCoverHolder = "./resources/site/Cover_PlaceHolder.png";
+    }
+}
